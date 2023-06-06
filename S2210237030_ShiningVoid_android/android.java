8dd111644603ec48d88e66349c56b6ecbb4fa4da
ShiningVoid <s2210237010@fhooe.at>
2023-06-06 17:31:52 +0200

    Dieser commit

--- S2210237030_ShiningVoid_android/android.java
+++ S2210237030_ShiningVoid_android/android.java
@@ -5,13 +5,22 @@ public void thisIsANewMethod() {
 }
 
 public class android {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hallo");
-        System.out.println("Penis");
+        System.out.println("Denis");
 	}
+
+	public static int UpCount(){
+		int x = 0;
+		for (int i = 0; i < 10; i++) {
+			x += i;
+		}
+		return x;
+	}
+
 
 }
 
-	
+
 >>>>>>> c02047c3cce6df6de3a1d710f034d0468bdf6ec5
